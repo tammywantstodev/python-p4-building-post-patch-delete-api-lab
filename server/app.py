7b755e9e372b2a3d4c2f4157be7762f858badcb0
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-03-30 14:53:25 +0300

    saving changes

--- server/app.py
+++ server/app.py
@@ -23,13 +23,28 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+
+@app.route('/bakeries/<int:id>',methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    if request.method == 'GET':
+        bakery_dict = bakery.to_dict()
+        return make_response(bakery_dict, 200)
 
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+            db.session.add(bakery)
+            db.session.commit()
+
+            bakery_dict=bakery.to_dict()
+
+            response= make_response(
+                bakery_dict,
+                200
+            )
+            return response
+       
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
@@ -44,6 +59,47 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods/<int:id>', methods=["DELETE"])
+def delete_baked_good(id):
+    baked_good = db.session.query(BakedGood).filter_by(id=id).first()
+    
+    if baked_good:
+        db.session.delete(baked_good)
+        db.session.commit()
+    response_body = {
+        "delete_successful": True,
+        "message": "Review deleted."
+    }
+
+    response = make_response(
+        response_body,
+        200
+    )
+
+    return response
+
+
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    name = request.form.get("name")
+    price = request.form.get("price")
+    bakery_id = request.form.get("bakery_id")
+
+    new_baked_good = BakedGood(
+        name=name,
+        price=price,
+        bakery_id=int(bakery_id)
+    )
+
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    response = make_response(new_baked_good.to_dict(), 201)
+    return response
+
+   
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
